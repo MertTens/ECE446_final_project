8eebe640ba894ae72da345a7ef24d5b36ea0cd5e
MertTens <amertens114@gmail.com>
2019-09-14 14:41:42 -0400

    changed permissions and made files easier to run

---
+++ audio_output_test.py
@@ -0,0 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+import pyaudio
+import numpy as np
+
+RATE = 44100
+CHUNK = 1024
+
+
+p = pyaudio.PyAudio()
+
+stream.stop_stream()
+stream.close()
+p.terminate()
