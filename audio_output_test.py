d4564aad267b1cb0c2aca4a5a13608112edb980a
MertTens <amertens114@gmail.com>
2019-09-15 01:21:04 -0400

    audio input to output

--- audio_output_test.py
+++ audio_output_test.py
@@ -8,6 +8,12 @@ CHUNK = 1024
 
 p = pyaudio.PyAudio()
 
+player = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+for i in range(int(10 * (RATE/CHUNK))):
+    player.write(np.fromstring(stream.read(CHUNK),dtype=np.int16))
+
 stream.stop_stream()
 stream.close()
 p.terminate()
