4f0ea8b9ac6f5d175cd953f3d7f6521d799385bd
vivian <vivian.m.tao@gmail.com>
2019-09-26 22:37:00 -0400

    :sparkles: vid read in RGB values

--- video_stream_test.py
+++ video_stream_test.py
@@ -1,1 +1,27 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+import numpy.core.multiarray
+import time
+import math
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    print(ret)
+    # Represents image
+    print(frame)
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
